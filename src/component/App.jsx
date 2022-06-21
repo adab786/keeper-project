a9d5ce0ce4ce095ea8d5b168046ef63c9ec8ffc8
adab786 <siddiquiadab@eng.rizvi.edu.in>
2022-06-21 16:17:29 +0530

    image

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-
+import Image from "./Image";
 function App() {
   return (
     <div>
       <Header />
       <Note />
       <Footer />
+      <Image />
     </div>
   );
 }
---
+++ src/component/Image.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+function Image() {
+  const pos = {
+    position: "absolute",
+    textAlign: "center",
+    top: "150px",
+  };
+  return (
+    <div>
+      <h2>hola amigos </h2>
+      <img style={pos} src="https://picsum.photos/500" alt="iameg"></img>
+    </div>
+  );
+}
+
+export default Image;
